428a1c6da510550abdc86c9424f7069e62a04d5e
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-12-11 05:57:04 +0530

    mt6737-common: Initial tree

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,169 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# inherit from the proprietary version
+-include vendor/motorola/mt6737-common/BoardConfigVendor.mk
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6737m
+TARGET_NO_BOOTLOADER := true
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := mt6737m
+
+# Recovery
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Kernel
+ARCH := arm
+TARGET_KERNEL_ARCH := arm
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+BOARD_KERNEL_BASE := 0x40000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_RAMDISK_OFFSET := 0x04000000
+BOARD_TAGS_OFFSET := 0xE000000
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,32N2 androidboot.selinux=permissive
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_TAGS_OFFSET)
+
+# make_ext4fs requires numbers in dec format
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2432696320
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 4698144768
+BOARD_CACHEIMAGE_PARTITION_SIZE := 419430400
+BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_KMODULES := true
+
+# Disable memcpy opt (for audio libraries)
+TARGET_CPU_MEMCPY_OPT_DISABLE := true
+
+# Flags
+BOARD_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+BOARD_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
+
+# Graphics
+BOARD_EGL_CFG := /vendor/motorola/mt6737-common/vendor/lib/egl/egl.cfg
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+USE_OPENGL_RENDERER := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+MTK_HWC_SUPPORT := yes
+MTK_HWC_VERSION := 1.4.1
+MTK_GPU_VERSION := mali midgard r7p0
+
+# Mediatek support
+BOARD_USES_MTK_HARDWARE:=true
+
+# Camera
+USE_CAMERA_STUB := true
+
+# Boot animation
+TARGET_BOOTANIMATION_MULTITHREAD_DECODE := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+
+# Audio
+BOARD_USES_MTK_AUDIO := true
+
+# CMHW
+BOARD_USES_CYANOGEN_HARDWARE := true
+BOARD_HARDWARE_CLASS := device/motorola/mt6737-common/cmhw
+
+# Fix video autoscaling on old OMX decoders
+TARGET_OMX_LEGACY_RESCALING := true
+
+# Charger
+BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
+
+# RIL
+BOARD_RIL_CLASS := ../../../device/motorola/mt6737-common/ril/
+
+# GPS
+BOARD_GPS_LIBRARIES :=true
+BOARD_CONNECTIVITY_MODULE := MT6625
+BOARD_MEDIATEK_USES_GPS := true
+
+# Wireless
+BOARD_WLAN_DEVICE := MediaTek
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_mt66xx
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_mt66xx
+WIFI_DRIVER_FW_PATH_PARAM := /dev/wmtWifi
+WIFI_DRIVER_FW_PATH_AP := AP
+WIFI_DRIVER_FW_PATH_STA := STA
+WIFI_DRIVER_FW_PATH_P2P := P2P
+WIFI_DRIVER_STATE_CTRL_PARAM := /dev/wmtWifi
+WIFI_DRIVER_STATE_ON := 1
+WIFI_DRIVER_STATE_OFF := 0
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+# Charger
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+
+# Fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_BLUETOOTH_BDROID_HCILP_INCLUDED := 0
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/motorola/mt6737-common/bluetooth
+
+TARGET_LDPRELOAD += mtk_symbols.so
+
+# Recovery
+TARGET_RECOVERY_FSTAB := device/motorola/mt6737-common/rootdir/recovery.fstab
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# TWRP stuff
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file
+TW_MAX_BRIGHTNESS := 255
+TW_EXCLUDE_SUPERSU := true
+TW_INCLUDE_FB2PNG := true
+TW_NO_CPU_TEMP := true
+TW_REBOOT_BOOTLOADER := true
+TW_REBOOT_RECOVERY := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_EXCLUDE_SUPERSU := true
+TW_USE_TOOLBOX := true
+
+TARGET_SYSTEM_PROP := device/motorola/mt6737-common/system.prop
+TARGET_SPECIFIC_HEADER_PATH := device/motorola/mt6737-common/include
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.zygote=zygote32
+
+# Sepolicy
+BOARD_SEPOLICY_DIRS := \
+       device/motorola/mt6737-common/sepolicy
+
+# Seccomp filter
+BOARD_SECCOMP_POLICY += device/motorola/mt6737-common/seccomp
